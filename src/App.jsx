7b14f8099eec4993ef4790aa22ba12328aee3a69
Vansh Chawla <vanshchawla019@gmail.com>
2026-01-02 23:43:46 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
   }
 
   return (
-    <div className="h-screen lg:flex bg-black text-white">
+    <div className="sm:h-[90%] lg:h-screen lg:flex bg-black text-white">
       <form
         onSubmit={(e) => {
           submitHandler(e);
@@ -67,12 +67,12 @@ const App = () => {
 
       <div className="lg:w-1/2 lg:border-l-2 p-10">
         <h1 className="text-4xl font-bold">Recent Notes</h1>
-        <div className="flex flex-wrap justify-start items-start gap-5 mt-6 h-[90%] overflow-auto">
+        <div id="scroll-container" className="flex flex-wrap justify-start items-start gap-5 mt-6 lg:h-[90%] overflow-auto">
           {task.map(function (elem, idx) {
             return (
               <div
                 key={idx}
-                className="flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]"
+                className="flex justify-between flex-col items-start relative h-52 sm:w-36 lg:w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]"
               >
                 <div>
                   <h3 className="leading-tight text-xl font-bold">
--- src/components/Section1/Section1.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Section1 = () => {
-  return (
-    <div className='h-screen w-full bg-blue-950'>Section1</div>
-  )
-}
-
-export default Section1
